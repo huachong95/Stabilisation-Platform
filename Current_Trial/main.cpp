e126d71c9f6fac0d594bdbcfb68797aa864758ed
HuaChong <huachong95@hotmail.sg>
2020-02-19 18:38:21 +0000

    added current_actual to Current Trial

--- Current_Trial/main.cpp
+++ Current_Trial/main.cpp
@@ -6,22 +6,43 @@
 #include "mbed.h"
 #include "platform/mbed_thread.h"
 
-
 // Blinking rate in milliseconds
-#define BLINKING_RATE_MS                                                    500
+#define BLINKING_RATE_MS 500
 RawSerial PC(USBTX, USBRX, 115200); // tx, rx for CoolTerm output
 
-int main()
-{
-    // Initialise the digital pin LED1 as an output
-    DigitalOut led(LED1);
-    AnalogIn CURRENT_Sensor(A1);
+float map(float in, float inMin, float inMax, float outMin, float outMax);
 
-    while (true) {
-        // led = !led;
-        // thread_sleep_for(BLINKING_RATE_MS);
-float current_temp=CURRENT_Sensor.read();
-printf("current_temp: %f \n\r",current_temp);
-thread_sleep_for(500);
-    }
+int main() {
+  // Initialise the digital pin LED1 as an output
+  DigitalOut led(LED1);
+  AnalogIn CURRENT_Sensor(A0);
+
+  while (true) {
+    // led = !led;
+    // thread_sleep_for(BLINKING_RATE_MS);
+    float current_temp = CURRENT_Sensor.read();
+    float current_actual = map(current_temp, 0.0, 1.0, -25, 25);
+    printf("current_temp: %f current_actual: %f \n\r", current_temp, current_actual);
+    thread_sleep_for(500);
+  }
+}
+
+float map(float in, float inMin, float inMax, float outMin,
+          float outMax) { // Function to scale the inputs to desired outputs
+  // check it's within the range
+  if (inMin < inMax) {
+    if (in <= inMin)
+      return outMin;
+    if (in >= inMax)
+      return outMax;
+  } else { // cope with input range being backwards.
+    if (in >= inMin)
+      return outMin;
+    if (in <= inMax)
+      return outMax;
+  }
+  // calculate how far into the range we are
+  float scale = (in - inMin) / (inMax - inMin);
+  // calculate the output.
+  return outMin + scale * (outMax - outMin);
 }
