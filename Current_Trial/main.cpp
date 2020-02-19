18576e763dac760cdc79b68f7827336c045082df
HuaChong <huachong95@hotmail.sg>
2020-02-19 02:08:45 +0000

    Serial Printing of Current Trial

--- Current_Trial/main.cpp
+++ Current_Trial/main.cpp
@@ -9,15 +9,19 @@
 
 // Blinking rate in milliseconds
 #define BLINKING_RATE_MS                                                    500
-
+RawSerial PC(USBTX, USBRX, 115200); // tx, rx for CoolTerm output
 
 int main()
 {
     // Initialise the digital pin LED1 as an output
     DigitalOut led(LED1);
+    AnalogIn CURRENT_Sensor(A1);
 
     while (true) {
-        led = !led;
-        thread_sleep_for(BLINKING_RATE_MS);
+        // led = !led;
+        // thread_sleep_for(BLINKING_RATE_MS);
+float current_temp=CURRENT_Sensor.read();
+printf("current_temp: %f \n\r",current_temp);
+thread_sleep_for(500);
     }
 }
