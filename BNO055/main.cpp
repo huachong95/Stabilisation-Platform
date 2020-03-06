5fd19470f51de74373b5ea5fa16d36ac96e57ac6
HuaChong <huachong95@hotmail.sg>
2020-03-06 19:35:33 +0000

    Stash

--- BNO055/main.cpp
+++ BNO055/main.cpp
@@ -2,6 +2,8 @@
  * Copyright (c) 2019 ARM Limited
  * SPDX-License-Identifier: Apache-2.0
  */
+#define IMU_INTERVAL 0.01 //100Hz
+#define SERIAL_PRINT_INTERVAL 0.01
 
 #include "BNO055.h"
 #include "mbed.h"
@@ -12,7 +14,18 @@
 RawSerial PC(USBTX, USBRX, 115200); // tx, rx for CoolTerm output
 DigitalOut led(LED1);
 BNO055 imu1(D14, D15);
+Ticker ANGLE_ISR;
+Ticker SERIAL_PRINT;
 
+float IMU_Pitch=0.0;
+float IMU_Roll=0.0;
+bool SERIAL_Print_Flag=0;
+bool IMU_Angle_Flag=0;
+
+
+void IMU_Angle();
+void SERIAL_Print();
+void SERIAL_Print_ISR();
 float map(float in, float inMin, float inMax, float outMin, float outMax);
 
 int main() {
@@ -21,6 +34,8 @@ int main() {
   led = 1;
   // Reset the BNO055
   imu1.reset();
+  ANGLE_ISR.attach(&IMU_Angle,IMU_INTERVAL);
+SERIAL_PRINT.attach(&SERIAL_Print_ISR, SERIAL_PRINT_INTERVAL);
   // Check that the BNO055 is connected and flash LED if not
   if (!imu1.check())
     while (true) {
@@ -42,15 +57,22 @@ int main() {
               imu1.ID.serial[i * 4 + 3]);
   }
   PC.printf("\r\n");
+      imu1.setmode(OPERATION_MODE_NDOF);
   while (true) {
-    imu1.setmode(OPERATION_MODE_NDOF);
+
     // imu1.get_calib();
     // imu1.get_angles();
-    imu1.get_lia();
-PC.printf("X: %5.2f, Y: %5.2f Z: %5.2f \n\r",imu1.lia.x,imu1.lia.y,imu1.lia.z);
-    // PC.printf("%0d %5.1f %5.1f %5.1f\r\n", imu1.calib, imu1.euler.roll,
-    //           imu1.euler.pitch, imu1.euler.yaw);
+    // imu1.get_lia();
+
     // thread_sleep_for(100);
+        if (SERIAL_Print_Flag) {
+      SERIAL_Print();
+      SERIAL_Print_Flag = 0;
+    }
+    if (IMU_Angle_Flag){
+        IMU_Angle();
+        IMU_Angle_Flag=0;
+    }
   }
 }
 
@@ -72,4 +94,19 @@ float map(float in, float inMin, float inMax, float outMin,
   float scale = (in - inMin) / (inMax - inMin);
   // calculate the output.
   return outMin + scale * (outMax - outMin);
+}
+void IMU_Angle(){
+        imu1.get_angles();
+    IMU_Pitch=imu1.euler.pitch;
+    IMU_Roll=imu1.euler.roll;
+
+}
+void SERIAL_Print_ISR() { SERIAL_Print_Flag = 1; }
+void IMU_Angle_ISR(){IMU_Angle_Flag=1;}
+void SERIAL_Print(){
+        PC.printf("Pitch: %5.2f, Roll: %5.2f \n\r",IMU_Pitch,IMU_Roll);
+// PC.printf("X: %5.2f, Y: %5.2f Z: %5.2f \n\r",imu1.lia.x,imu1.lia.y,imu1.lia.z);
+// PC.printf("X: %5.2f, Y: %5.2f Z: %5.2f \n\r",imu1.euler.pitch,imu1.euler.roll,imu1.euler.yaw);
+    // PC.printf("%0d %5.1f %5.1f %5.1f\r\n", imu1.calib, imu1.euler.roll,
+    //           imu1.euler.pitch, imu1.euler.yaw);
 }
